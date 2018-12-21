73e37d9cc623939955c77d786930f5e81c6f2183
SusanaM97 <susanaferreira97.csg@gmail.com>
2018-12-21 12:57:46 +0000

    pontape de saida
    pweb

---
+++ TP1819/TP1819/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TP1819.MvcApplication" Language="C#" %>
---
+++ TP1819/TP1819/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TP1819.Startup))]
+namespace TP1819
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
